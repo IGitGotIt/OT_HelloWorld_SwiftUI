43017f4138100c373406dc7fd42f18ccb5f64698
IGitGotIt <jshah.jshah@gmail.com>
2020-07-02 15:11:02 -0700

    Initial Commit

---
+++ OT_HelloWorld_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OT_HelloWorld_SwiftUI
+//
+//  Created by Jaideep Shah on 7/2/20.
+//  Copyright © 2020 Jaideep Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
